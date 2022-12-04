f6041a942471361c31aa8c96b62c118a77d4682c
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2022-12-04 14:57:45 -0300

    day4

---
+++ src/main/kotlin/D4.kt
@@ -0,0 +1,63 @@
+package main.kotlin
+
+object D4 : Solver {
+
+    data class Range(val from: Int, val to: Int) {
+        fun contains(other: Range): Boolean = from <= other.from && to >= other.to
+
+        fun overlaps(other: Range): Boolean {
+            return if (this.from <= other.from) this.to >= other.from
+            else other.from <= this.from && other.to >= this.from
+        }
+
+        companion object {
+            fun overlaps(line: String): Boolean {
+                val (l, r) = parse(line)
+                return l.overlaps(r)
+            }
+
+            fun contained(line: String): Boolean {
+                val (l, r) = parse(line)
+                return l.contains(r) || r.contains(l)
+            }
+
+            fun parse(line: String): Pair<Range, Range> {
+                return Regex("([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)").matchEntire(line)?.let {
+                    Pair(
+                        Range(it.groupValues[1].toInt(), it.groupValues[2].toInt()),
+                        Range(it.groupValues[3].toInt(), it.groupValues[4].toInt())
+                    )
+                }!!
+            }
+        }
+
+    }
+
+
+    override fun solve(input: List<String>): Any {
+        return input.map { Range.contained(it) }.count { it }
+    }
+
+    override fun solveb(input: List<String>): Any {
+        return input.count {  Range.overlaps(it) }
+    }
+
+
+}
+
+fun main() {
+    println(
+        D4.solveb(
+            """
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+    """.trimIndent()
+        )
+    )
+
+    println(D4.solveb("day4.txt"))
+}
