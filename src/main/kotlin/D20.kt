cbe40b4a911bd5b37f02e80749dcf88443dcaa10
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2022-12-20 22:23:17 -0300

    day20 part b

--- src/main/kotlin/D20.kt
+++ src/main/kotlin/D20.kt
@@ -11,9 +11,9 @@ object D20 : Solver {
             "4"
 
 
-    val sentinel = ListNode(Int.MIN_VALUE)
+    val sentinel = ListNode(Long.MIN_VALUE)
 
-    data class ListNode(val num: Int) {
+    data class ListNode(val num: Long) {
         var previous: ListNode = sentinel
         var next: ListNode = sentinel
 
@@ -53,7 +53,6 @@ object D20 : Solver {
         }
 
         fun walk(n: Int): ListNode {
-//            if(n == 0) this else if(n < 0) this.previous.walk(n + 1) else this.next.walk(n - 1)
             var node = this
             if (n > 0) repeat(n) { node = node.next }
             else repeat(-n) { node = node.previous }
@@ -70,20 +69,42 @@ object D20 : Solver {
                 val prev = this.previous
                 this.dettach()
                 if(!next.isCoherent() || !prev.isCoherent()) throw RuntimeException()
-//                this.moveAfter(prev.walk(this.num))
-                this.insertAfter(prev.walk(this.num))
+                this.insertAfter(prev.walk(this.num.toInt()))
             } else if(this.num > 0){
                 val next = this.next
                 val prev = this.previous
                 this.dettach()
                 if(!next.isCoherent() || !prev.isCoherent()) throw RuntimeException()
-                this.insertAfter(next.walk(this.num - 1))
-//                this.moveAfter(this.walk(this.num))
+                this.insertAfter(next.walk((this.num - 1).toInt()))
             }
             if(!isCoherent() || !this.previous.isCoherent() || !this.next.isCoherent()) {
                 throw RuntimeException("fuuuu")
             }
         }
+
+        fun swapB(max: Int) {
+            if(!isCoherent() || !this.previous.isCoherent() || !this.next.isCoherent()) {
+                throw RuntimeException("fuuuu")
+            }
+
+            if(this.num < 0) {
+                val next = this.next
+                val prev = this.previous
+                this.dettach()
+                if(!next.isCoherent() || !prev.isCoherent()) throw RuntimeException()
+                this.insertAfter(prev.walk(this.num.mod(max - 1)))
+            } else if(this.num > 0){
+                val next = this.next
+                val prev = this.previous
+                this.dettach()
+                if(!next.isCoherent() || !prev.isCoherent()) throw RuntimeException()
+                this.insertAfter(next.walk((this.num - 1).mod(max - 1)))
+            }
+            if(!isCoherent() || !this.previous.isCoherent() || !this.next.isCoherent()) {
+                throw RuntimeException("fuuuu")
+            }
+        }
+
 
         fun printMe() {
             var n = this
@@ -101,8 +122,9 @@ object D20 : Solver {
 
     }
 
+    val decryptionKey = 811589153L
     override fun solve(input: List<String>): Any {
-        val listNode = input.map { ListNode(it.toInt()) }
+        val listNode = input.map { ListNode(it.toLong()) }
         listNode.windowed(2).forEach { (a, b) -> a.next = b; b.previous = a }
         listNode.first().previous = listNode.last()
         listNode.last().next = listNode.first()
@@ -113,7 +135,30 @@ object D20 : Solver {
         }
         if(!listNode.all { it.isCoherent() }) throw RuntimeException("bug")
 
-        val node0 = listNode.find { it.num == 0 }!!
+        val node0 = listNode.find { it.num == 0L }!!
+//        node0.printMe()
+        val node1 = node0.walk(1000)
+        val node2 = node0.walk(2000)
+        val node3 = node0.walk(3000)
+        return node1.num + node2.num + node3.num
+    }
+
+    override fun solveb(input: List<String>): Any {
+        val listNode = input.map { ListNode(it.toLong() * decryptionKey) }
+        listNode.windowed(2).forEach { (a, b) -> a.next = b; b.previous = a }
+        listNode.first().previous = listNode.last()
+        listNode.last().next = listNode.first()
+        repeat(10) {
+            listNode.forEach {node ->
+//            println("swapping ${it.num}")
+//                node.swap()
+                node.swapB(listNode.size)
+//            if(!listNode.all { it.isCoherent() }) throw RuntimeException("bug")
+            }
+        }
+        if(!listNode.all { it.isCoherent() }) throw RuntimeException("bug")
+
+        val node0 = listNode.find { it.num == 0L }!!
 //        node0.printMe()
         val node1 = node0.walk(1000)
         val node2 = node0.walk(2000)
@@ -126,8 +171,10 @@ object D20 : Solver {
 
 fun main() {
 
-//    D20.solveSample(3)
-    if(D20.solve("day20.txt") != 8764) throw RuntimeException()
+//    D20.solveSample(3L)
+//    if(D20.solve("day20.txt") != 8764) throw RuntimeException()
 
+    D20.solveSampleB(1623178306L)
+    D20.solveb("day20.txt")
 //    D20.solve(listOf("5", "0", "1"))
 }
