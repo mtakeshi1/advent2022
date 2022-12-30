30a850b8f093ba89887cf5a6dc7ffb631baebaf5
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2022-12-30 18:45:20 -0300

    beginning of day20b. Still need to fill the complete picture, but it looks like its not ambiguous

--- src/main/kotlin/y2020/D20.kt
+++ src/main/kotlin/y2020/D20.kt
@@ -1,10 +1,7 @@
 package y2020
 
 import advent2020.Solver2020
-import y2022.println
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.util.zip.GZIPOutputStream
+import kotlin.math.sqrt
 
 object D20 : Solver2020 {
     // upside down
@@ -13,12 +10,16 @@ object D20 : Solver2020 {
 
     data class Cube(
         val id: Long,
-        val north: List<Char>,
-        val east: List<Char>,
-        val south: List<Char>,
-        val west: List<Char>
+        val body: List<List<Char>>
     ) {
-        fun sides() = listOf(north, east, west, south)
+        fun north(): List<Char> = body.first()
+        fun east(): List<Char> = body.map { it.last() }
+        fun south(): List<Char> = body.last()
+        fun west(): List<Char> = body.map { it.first() }
+
+        fun column(c: Int) = body.map { it[c] }
+
+        fun sides() = listOf(north(), east(), west(), south())
     }
 
     fun interface CubeTransformation {
@@ -31,26 +32,25 @@ object D20 : Solver2020 {
 
     val eastToWest = CubeTransformation {
         it.copy(
-            north = it.north.reversed(),
-            south = it.south.reversed(),
-            east = it.west,
-            west = it.east
+            body = it.body.map { it.reversed() }
+//            north = it.north.reversed(),
+//            south = it.south.reversed(),
+//            east = it.west,
+//            west = it.east
         )
     }
     val upsideDown = CubeTransformation {
         it.copy(
-            east = it.east.reversed(),
-            west = it.west.reversed(),
-            north = it.south,
-            south = it.north
+            body = it.body.reversed()
+//            east = it.east.reversed(),
+//            west = it.west.reversed(),
+//            north = it.south,
+//            south = it.north
         )
     }
-    val clockWiseRotation = CubeTransformation {
-        it.copy(
-            north = it.west.reversed(),
-            east = it.north,
-            south = it.east,
-            west = it.south.reversed()
+    val clockWiseRotation = CubeTransformation { cube ->
+        cube.copy(
+            body = (0 until cube.body.size).map { c -> cube.column(c) }
         )
     }
 
@@ -66,19 +66,7 @@ object D20 : Solver2020 {
 
     private fun parseCube(cube: List<String>): Cube {
         val id = cube.first().anyInt()!!.toLong()
-        val north = cube[1].toList()
-        val south = cube.last().toList()
-        val east = cube.drop(1).map { it.last() }
-        val west = cube.drop(1).map { it.first() }
-        return Cube(id, north, east, south, west)
-    }
-
-    fun compress(file: File): Double {
-        val bout = ByteArrayOutputStream()
-        val gout = GZIPOutputStream(bout)
-        file.inputStream().copyTo(gout)
-        gout.close()
-        return bout.toByteArray().size.toDouble() / file.length()
+        return Cube(id, cube.drop(1).map { it.toList() })
     }
 
     override fun solve(input: List<String>): Any {
@@ -88,33 +76,71 @@ object D20 : Solver2020 {
         allCubeRotations.forEach { cube ->
             cube.sides().forEach { side -> map.computeIfAbsent(side) { HashSet() }.add(cube.id) }
         }
-        val topLeft= cubes.filter { cube ->
+        val topLeft = cubes.filter { cube ->
             val rotated = allTransformations.map { tx -> tx.transform(cube) } + cube
-            rotated.any{ c -> map[c.north]!!.size == 1 && map[c.west]!!.size == 1}
+            rotated.any { c -> map[c.north()]!!.size == 1 && map[c.west()]!!.size == 1 }
         }.map { it.id }
         val topRight = cubes.filter { cube ->
             val rotated = allTransformations.map { tx -> tx.transform(cube) } + cube
-            rotated.any{ c -> map[c.north]!!.size == 1 && map[c.east]!!.size == 1}
-        }.map{it.id}
+            rotated.any { c -> map[c.north()]!!.size == 1 && map[c.east()]!!.size == 1 }
+        }.map { it.id }
         val botRight = cubes.filter { cube ->
             val rotated = allTransformations.map { tx -> tx.transform(cube) } + cube
-            rotated.any{ c -> map[c.south]!!.size == 1 && map[c.east]!!.size == 1}
-        }.map{it.id}
+            rotated.any { c -> map[c.south()]!!.size == 1 && map[c.east()]!!.size == 1 }
+        }.map { it.id }
         val botLeft = cubes.filter { cube ->
             val rotated = allTransformations.map { tx -> tx.transform(cube) } + cube
-            rotated.any{ c -> map[c.south]!!.size == 1 && map[c.west]!!.size == 1}
-        }.map{it.id}
+            rotated.any { c -> map[c.south()]!!.size == 1 && map[c.west()]!!.size == 1 }
+        }.map { it.id }
         println(topLeft)
         println(topRight)
         println(botLeft)
         println(botRight)
 //        map.mapValues { it.value.size }.forEach{println(it)}
-        return topRight.reduce{a, b-> a * b}
+        return topRight.reduce { a, b -> a * b }
     }
 
+    override fun solveb(input: List<String>): Any {
+        val cubes = input.partitionOnEmpty().map { parseCube(it) }.associateBy { it.id }
+        val map = mutableMapOf<List<Char>, MutableSet<Long>>()
+        val allCubeRotations =
+            cubes.values.flatMap { cube -> allTransformations.map { tx -> tx.transform(cube) } + cube }
+        allCubeRotations.forEach { cube ->
+            cube.sides().forEach { side -> map.computeIfAbsent(side) { HashSet() }.add(cube.id) }
+        }
+        val corners = cubes.values.filter { cube ->
+            val rotated = allTransformations.map { tx -> tx.transform(cube) } + cube
+            rotated.any { c -> map[c.north()]!!.size == 1 && map[c.west()]!!.size == 1 }
+        }
+        check(corners.size == 4)
+        val dim = sqrt(cubes.size.toDouble()).toInt()
+        val picture = Array(dim) { Array(dim) { Cube(0, emptyList()) } }
+
+        val availableCubes = mutableSetOf<Cube>()
+        cubes.values.forEach(availableCubes::add)
+        availableCubes.removeAll(corners)
+
+        picture[0][0] = corners.first()
+        fun fill( row: Int,  col: Int) {
+            availableCubes.filter { cube ->
+                val rotated = allTransformations.map { tx -> tx.transform(cube) } + cube
+                rotated.any {
+                        c -> map[c.north()]!!.size == 1 && c.west() == corners.first().east()
+                }
+            }
+        }
+        val nextCube = availableCubes.filter { cube ->
+            val rotated = allTransformations.map { tx -> tx.transform(cube) } + cube
+            rotated.any { c -> map[c.north()]!!.size == 1 && c.west() == corners.first().east() }
+        }
+
+        check(nextCube.size == 1)
+
+        return 1
+    }
 }
 
 fun main() {
 //    D20.solveSample(20899048083289L)
-    D20.solve("day20.txt")
+    D20.solveb("day20.txt")
 }
